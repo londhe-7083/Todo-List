628cffc02f18bc70cf090f56199b9b2a46692053
londhe-7083 <shubhamlondhe1583@gmail.com>
2023-04-01 00:52:28 +0530

    adding components

--- to do-list/to-do-list/src/App.js
+++ to do-list/to-do-list/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Todo from "./MyComponents/Todo";
+import {Footer} from "./MyComponents/Footer";
 
 function App() {
   return (
      <>
      <Header />
+     <Todos />
+     <Footer />
      </>
 
   );
